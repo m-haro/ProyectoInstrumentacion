bd4a8c383f81bc297e4381751c6bc34d267c4273
m-haro <m-haro@hotmail.com>
2013-02-23 10:29:58 -0500

    Subiendo sitio completo

---
+++ img/mencaud.js
@@ -0,0 +1,77 @@
+var clicked=""
+var gtype=".png"
+var selstate="_over"
+if (typeof(loc)=="undefined" || loc==""){
+	var loc=""
+	if (document.body&&document.body.innerHTML){
+		var tt = document.body.innerHTML.toLowerCase();
+		var last = tt.indexOf("mencaud.js\"");
+		if (last>0){
+			var first = tt.lastIndexOf("\"", last);
+			if (first>0 && first<last) loc = document.body.innerHTML.substr(first+1,last-first-1);
+		}
+	}
+}
+
+document.write("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr>");
+tr(false);
+writeButton(loc+"../cau/","indeq.htm","mencaud_b1",93,28,"Ecuaciones de Caudal","mainFrame",0);
+writeButton(loc+"../cau/","indpost.htm","mencaud_b2",93,28,"Desplasamiento positivo","mainFrame",0);
+writeButton(loc+"../cau/","indrota.htm","mencaud_b3",93,28,"Rotámetros","mainFrame",0);
+writeButton(loc+"../cau/","indifer.htm","mencaud_b4",93,28,"Presión Diferencial","mainFrame",0);
+writeButton(loc+"../cau/","indmag.htm","mencaud_b5",93,28,"Magnéticos","mainFrame",0);
+writeButton(loc+"../cau/","indultra.htm","mencaud_b6",93,28,"Ultrasonido","mainFrame",0);
+writeButton(loc+"../cau/","indturb.htm","mencaud_b7",93,28,"Turbina","mainFrame",0);
+tr(true);
+document.write("</tr></table>")
+loc="";
+
+function tr(b){if (b) document.write("<tr>");else document.write("</tr>");}
+
+function turn_over(name) {
+	if (document.images != null && clicked != name) {
+		document[name].src = document[name+"_over"].src;
+	}
+}
+
+function turn_off(name) {
+	if (document.images != null && clicked != name) {
+		document[name].src = document[name+"_off"].src;
+	}
+}
+
+function reg(gname,name)
+{
+if (document.images)
+	{
+	document[name+"_off"] = new Image();
+	document[name+"_off"].src = loc+gname+gtype;
+	document[name+"_over"] = new Image();
+	document[name+"_over"].src = loc+gname+"_over"+gtype;
+	}
+}
+
+function evs(name){ return " onmouseover=\"turn_over('"+ name + "')\" onmouseout=\"turn_off('"+ name + "')\""}
+
+function writeButton(urld, url, name, w, h, alt, target, hsp)
+{
+	gname = name;
+	while(typeof(document[name])!="undefined") name += "x";
+	reg(gname, name);
+	tr(true);
+	document.write("<td>");
+	if (alt != "") alt = " alt=\"" + alt + "\"";
+	if (target != "") target = " target=\"" + target + "\"";
+	if (w > 0) w = " width=\""+w+"\""; else w = "";
+	if (h > 0) h = " height=\""+h+"\""; else h = "";	
+	if (url != "") url = " href=\"" + urld + url + "\"";
+	
+	document.write("<a " + url + evs(name) + target + ">");	
+	
+	if (hsp == -1) hsp =" align=\"right\"";
+	else if (hsp > 0) hsp = " hspace=\""+hsp+"\"";
+	else hsp = "";
+	
+	document.write("<img src=\""+loc+gname+gtype+"\" name=\"" + name + "\"" + w + h + alt + hsp + " border=\"0\" /></a></td>");
+	tr(false);
+}
---
+++ img/mnutmp.js
@@ -0,0 +1,76 @@
+var clicked=""
+var gtype=".png"
+var selstate="_over"
+if (typeof(loc)=="undefined" || loc==""){
+	var loc=""
+	if (document.body&&document.body.innerHTML){
+		var tt = document.body.innerHTML.toLowerCase();
+		var last = tt.indexOf("mnutmp.js\"");
+		if (last>0){
+			var first = tt.lastIndexOf("\"", last);
+			if (first>0 && first<last) loc = document.body.innerHTML.substr(first+1,last-first-1);
+		}
+	}
+}
+
+document.write("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr>");
+tr(false);
+writeButton(loc+"../tmt/","ixe.htm","mnutmp_b1",83,28,"Ecuaciones","mainFrame",0);
+writeButton(loc+"../tmt/","idil.htm","mnutmp_b2",83,28,"De Dilatación","mainFrame",0);
+writeButton(loc+"../tmt/","ipar.htm","mnutmp_b3",83,28,"Termopares","mainFrame",0);
+writeButton(loc+"../tmt/","irs.htm","mnutmp_b4",83,28,"De Resistencia","mainFrame",0);
+writeButton(loc+"../tmt/","ipi.htm","mnutmp_b5",83,28,"Pirómetros de Radiación","mainFrame",0);
+writeButton(loc+"../tmt/","ire.htm","mnutmp_b6",83,28,"Sistemas de Relleno","mainFrame",0);
+tr(true);
+document.write("</tr></table>")
+loc="";
+
+function tr(b){if (b) document.write("<tr>");else document.write("</tr>");}
+
+function turn_over(name) {
+	if (document.images != null && clicked != name) {
+		document[name].src = document[name+"_over"].src;
+	}
+}
+
+function turn_off(name) {
+	if (document.images != null && clicked != name) {
+		document[name].src = document[name+"_off"].src;
+	}
+}
+
+function reg(gname,name)
+{
+if (document.images)
+	{
+	document[name+"_off"] = new Image();
+	document[name+"_off"].src = loc+gname+gtype;
+	document[name+"_over"] = new Image();
+	document[name+"_over"].src = loc+gname+"_over"+gtype;
+	}
+}
+
+function evs(name){ return " onmouseover=\"turn_over('"+ name + "')\" onmouseout=\"turn_off('"+ name + "')\""}
+
+function writeButton(urld, url, name, w, h, alt, target, hsp)
+{
+	gname = name;
+	while(typeof(document[name])!="undefined") name += "x";
+	reg(gname, name);
+	tr(true);
+	document.write("<td>");
+	if (alt != "") alt = " alt=\"" + alt + "\"";
+	if (target != "") target = " target=\"" + target + "\"";
+	if (w > 0) w = " width=\""+w+"\""; else w = "";
+	if (h > 0) h = " height=\""+h+"\""; else h = "";	
+	if (url != "") url = " href=\"" + urld + url + "\"";
+	
+	document.write("<a " + url + evs(name) + target + ">");	
+	
+	if (hsp == -1) hsp =" align=\"right\"";
+	else if (hsp > 0) hsp = " hspace=\""+hsp+"\"";
+	else hsp = "";
+	
+	document.write("<img src=\""+loc+gname+gtype+"\" name=\"" + name + "\"" + w + h + alt + hsp + " border=\"0\" /></a></td>");
+	tr(false);
+}
